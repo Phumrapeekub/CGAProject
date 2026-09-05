bc5015cc5fedc4d75b340cf0a1dc4d22a133c9be
Phumrapee <66025773@up.ac.th>
2026-09-05 19:19:16 +0700

    Change host to localhost to avoid confusion with 192 IPs

--- app.py
+++ app.py
@@ -35,4 +35,4 @@ def supabase_status():
     return jsonify(result)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=int(os.environ.get("PORT", 8080)), host="0.0.0.0")
+    app.run(debug=True, port=int(os.environ.get("PORT", 8080)))
